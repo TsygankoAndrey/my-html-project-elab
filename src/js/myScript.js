feeac33ce73f11b001ae16c1fc29b1569e0d6320
TsygankoAndrey <atsyganko@gmail.com>
2024-08-28 20:07:19 +0300

    32.46

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -6,6 +6,36 @@
 переменные
 для объявления переменной вводим
 let userName = "Марк";
-
 */
+
+{
+    let userName = "Bob";
+    console.log(userName);
+
+    var someNumber = 10;
+    console.log(someNumber);
+}
+
+// console.log(userName);
+console.log(someNumber);
+
+//============ типы переменных ============
+// по умолчанию используем const,
+// когда явно понятно, что переменная будет менять занчение, тогда let
+
+//============== типы данных ===============
+ /*let userName = 'Mark'; // строка - в кавычках - string
+ let age = 30; //число - без кавычек - number
+ let isMarried = true; //логические значения true/ false - Boolean*/
+
+ //динамическая типизация
+ let someVariable = 'some value'; //someVariable типа string
+ someVariable = 50; //someVariable типа number
+ someVariable = true; //someVariable типа Boolean   
+
+ console.log(someVariable);
+
+ let age = 32;
+ age = null;
+
 
